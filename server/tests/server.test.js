94a29c49479b7ec92f2205c69567ada6a0c2ac19
PRANAVBHALE <bhalepranav25@gmail.com>
2017-11-24 02:23:37 +0530

    ADD GET ROUTE TEST SUITE

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,20 +5,32 @@ const request=require('supertest')
 const {app}=require('./../server')
 const{Todo}=require('./../models/todo')
 
+
+const todos=[{
+  text:'First todo task'
+},{
+  text:'second todo task'
+},{
+  text:'third todo task'
+}]
+
+
 beforeEach((done)=>{
-  Todo.remove({}).then(()=>done())
+  Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos)
+  }).then(()=>done())
 })
 
 describe('POST /todos',()=>{
   it('should create a new todo',(done)=>{
-    var text = 'Test todo text'
+  //  var text = 'Test todo text'
 
     request(app)
     .post('/todos')
-    .send({text})
-    .expect(200)
+    .send({})
+    .expect(400)
     .expect((res)=>{
-      expect(res.body.text).toBe(text);
+      expect(res.body.text).toBe();
       done()
 
     })
@@ -27,9 +39,9 @@ describe('POST /todos',()=>{
         return done(err)
       }
 
-      Todo.find().then((todos)=>{
-        expect(todos.length).toBe(1)
-        expect(todos[0].text).toBe(text)
+      Todo.find(text).then((todos)=>{
+        expect(todos.length).toBe(4)
+        expect(todos[0].text).toBe()
       }).catch((e)=>done(e))
     })
   })
@@ -47,8 +59,21 @@ describe('POST /todos',()=>{
     }
 
     Todo.find().then((todos)=>{
-      expect(todos.length).toBe(0)
+      expect(todos.length).toBe(3)
     }).catch((e)=>done(e))
   })
 })
+})
+
+
+describe('GET/todos',()=>{
+  it('should get all to dos',(done)=>{
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todos.length).toBe(3)
+    })
+    .end(done)
+  })
 })
