444c17039b36bff93f58589f9944f159ec6cf27f
PRANAVBHALE <bhalepranav25@gmail.com>
2017-11-24 16:08:56 +0530

    GET TODOS BY :ID ROUTE

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
+
+
 var express=require('express')
 var bodyParser=require('body-parser')
+const{ObjectID}=require('mongodb')
+
 
 var {mongoose}=require('./db/mongoose.js')
 var {Todo}=require('./models/todo.js')
@@ -34,6 +38,26 @@ app.get('/todos',(req,res)=>{
 
   })
 })
+
+//GET/todos/12345 (:id) ie dynamic id request
+
+app.get('/todos/:id',(req,res)=>{
+//  res.send(req.params)
+var id = req.params.id
+if(!ObjectID.isValid(id)){
+  // console.log('Id not valid');
+  return res.status(404).send()
+}
+
+Todo.findById(id).then((todo)=>{
+  if(!todo){
+    return res.status(404).send()
+  }
+  res.send({todo});
+}).catch((e)=>{
+  res.status(400).send()
+})
+});
 
 app.listen(3000,()=>{
   console.log('Server availavble on port 3000');
