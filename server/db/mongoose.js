3fab022cc28ff77c3ecb862b90ad44492238d7bc
PRANAVBHALE <bhalepranav25@gmail.com>
2017-11-25 00:27:58 +0530

    HEROKU DEPLOYMENT

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
 
+    let db = {
+      localhost: 'mongodb://localhost:27017/TodoApp',
+      mlab: 'mongodb://pranav:bhale@ds117336.mlab.com:17336/todoapp'
+    };
+
+
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp',{ useMongoClient: true })
+mongoose.connect(db.localhost||db.mlab,{ useMongoClient: true })
 
 module.export={mongoose}
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ var {User}=require('./models/user.js')
 
 var app = express()
 
-
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 
@@ -59,8 +59,8 @@ Todo.findById(id).then((todo)=>{
 })
 });
 
-app.listen(3000,()=>{
-  console.log('Server availavble on port 3000');
+app.listen(port,()=>{
+  console.log(`Server availavble on port ${port}`);
 })
 
 
