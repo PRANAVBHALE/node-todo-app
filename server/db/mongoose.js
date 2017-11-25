18e3c440e3f4fdc10219dc1ea29ef105de4e9bd8
PRANAVBHALE <bhalepranav25@gmail.com>
2017-11-25 13:59:04 +0530

    HEROKU DEPLYOMENT 5

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 
-    let db = {
-      localhost: 'mongodb://localhost:27017/TodoApp',
-      mlab: 'mlab://pranav:bhale@ds117336.mlab.com:17336/todoapp'
-    };
+    // let db = {
+    //   localhost: ,
+    //   mlab: 'mlab://pranav:bhale@ds117336.mlab.com:17336/todoapp'
+    // };
 
 
 mongoose.Promise=global.Promise;
-mongoose.connect(db.localhost||db.mlab,{ useMongoClient: true })
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp',{ useMongoClient: true })
 
 module.export={mongoose}
