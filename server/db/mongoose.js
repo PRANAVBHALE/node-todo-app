41f646eb3573b47f21bd7742692d9f17552956e9
PRANAVBHALE <bhalepranav25@gmail.com>
2017-11-25 13:52:13 +0530

    HEROK DEPLOYMENT

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
     let db = {
       localhost: 'mongodb://localhost:27017/TodoApp',
-      mlab: 'mongodb://pranav:bhale@ds117336.mlab.com:17336/todoapp'
+      mlab: 'mlab://pranav:bhale@ds117336.mlab.com:17336/todoapp'
     };
 
 
