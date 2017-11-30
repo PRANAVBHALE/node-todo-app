4757f6fe164a834052a954a93d97fb8b7f7da57a
PRANAVBHALE <bhalepranav25@gmail.com>
2017-11-30 10:34:51 +0530

    DATABASE ENV SETUP

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
 
-    let db = {
-      localhost: 'mongodb://localhost:27017/TodoApp',
-      mlab: 'mongodb://pranav:bhale@ds117336.mlab.com:17336/todoapp'
-    };
+//     let db = {
+//       localhost: 'mongodb://localhost:27017/TodoApp',
+//       mlab: 'mongodb://pranav:bhale@ds117336.mlab.com:17336/todoapp'
+//     };
+//
+//
+// mongoose.Promise=global.Promise;
+// mongoose.connect(db.mla || db.localhost,{ useMongoClient: true })
 
-
-mongoose.Promise=global.Promise;
-mongoose.connect(db.mlab,{ useMongoClient: true })
+mongoose.Promise =global.Promise
+mongoose.connect(process.env.MONGODB_URI ,{ useMongoClient: true })
 
 module.export={mongoose}
+
+//process.env.NODE_ENV === 'test'
